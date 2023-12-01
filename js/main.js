776affb14fd8599001e22c1663096c14974675f1
Kevin <kevbao24@gmail.com>
2023-12-01 20:27:19 +0000

    Get references to HTML elements

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// Get references to HTML elements
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+const resultContainer = document.getElementById('resultContainer');
