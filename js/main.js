52c1fd4c8f305ba3ae0e08f88c6121b48f25bd94
Kevin <kevbao24@gmail.com>
2023-12-04 09:51:29 +0000

    add event listener

--- js/main.js
+++ js/main.js
@@ -2,3 +2,12 @@
 const searchInput = document.getElementById('searchInput');
 const searchButton = document.getElementById('searchButton');
 const resultContainer = document.getElementById('resultContainer');
+
+// Add event listener for the search button click
+searchButton.addEventListener('click', () => {
+  const searchTerm = searchInput.value.trim().toLowerCase(); // Get the trimmed and lowercased search term entered by the user
+
+  if (searchTerm !== '') {
+    const apiUrl = 'https://valorant-api.com/v1/weapons/skins';
+  }
+});
