b3b80c42e44b9ce38aa052d23b7f07261eee4d64
Kevin <kevbao24@gmail.com>
2023-12-05 10:10:30 +0000

    iterate through skins in the API

--- js/main.js
+++ js/main.js
@@ -16,6 +16,29 @@ searchButton.addEventListener('click', () => {
         const matchingSkins = data.data.filter((skin) => {
           return skin.displayName.toLowerCase().includes(searchTerm);
         });
+
+        if (matchingSkins.length > 0) {
+          resultContainer.innerHTML = '';
+          matchingSkins.forEach((skin) => {
+            const skinElement = document.createElement('div');
+            skinElement.classList.add('skin'); //
+
+            const skinName = document.createElement('h3');
+            skinName.textContent = skin.displayName;
+
+            const skinImage = document.createElement('img');
+            skinImage.src = skin.displayIcon;
+
+            skinElement.appendChild(skinName);
+            skinElement.appendChild(skinImage);
+
+            resultContainer.appendChild(skinElement);
+          });
+        } else {
+          resultContainer.innerHTML = 'No matching skins found';
+        }
       });
+  } else {
+    alert('Please enter a search term');
   }
 });
